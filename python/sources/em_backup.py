0d7685f46c7a7e3b1986949e889c17bed6cbec36
mugecxw-sys <mugecxw@gmail.com>
2026-07-23 16:20:55 +0800

    Update em_backup.py

--- python/sources/em_backup.py
+++ python/sources/em_backup.py
@@ -3,13 +3,14 @@ import akshare as ak
 
 def get_data():
 
-    df = ak.stock_sector_fund_flow_summary_em()
+
+    df = ak.stock_sector_fund_flow_summary()
 
 
     if df is None or len(df)==0:
 
         raise Exception(
-            "备用资金接口为空"
+            "备用接口返回为空"
         )
 
 
