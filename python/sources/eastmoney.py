86f0f107e7e450eaec41607e856f16ac4aaebb2f
mugecxw-sys <mugecxw@gmail.com>
2026-07-23 16:00:05 +0800

    Create eastmoney.py

---
+++ python/sources/eastmoney.py
@@ -0,0 +1,20 @@
+import akshare as ak
+
+
+def get_data():
+
+
+    df = ak.stock_sector_fund_flow_rank(
+        indicator="今日",
+        sector_type="概念资金流"
+    )
+
+
+    if df is None or len(df)==0:
+
+        raise Exception(
+            "东方财富返回为空"
+        )
+
+
+    return df
